510355618cb73190265ce70e38146d9022839819
Alexei Gladkikh <a.gladkikh@inforion.ru>
2019-03-18 17:16:42 +0300

    Added telegram library and src template

---
+++ src/main/kotlin/ru/inforion/lab403/utils/ytbot/app.kt
@@ -0,0 +1,16 @@
+package ru.inforion.lab403.utils.ytbot
+
+import ru.inforion.lab403.common.logging.logger
+import java.util.logging.Level
+
+
+class YouTrackTelegramBotStarter {
+    companion object {
+        private val log = logger(Level.ALL)
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+        }
+    }
+}
